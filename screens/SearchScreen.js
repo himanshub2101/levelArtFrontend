36d89eddfb3de8c12f15520813e7713cb09f80ad
anushkabawne <anushkabawne11@gmail.com>
2024-04-26 15:31:34 +0530

    add icons

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,18 +1,30 @@
-import React, { useState } from 'react';
-import { View, Text, TextInput, FlatList } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, TextInput, FlatList, StyleSheet, TouchableOpacity, Animated } from 'react-native';
+import { Feather } from '@expo/vector-icons'; // Import Feather icons from Expo
 import axios from 'axios';
 
 const SearchScreen = () => {
   const [searchQuery, setSearchQuery] = useState('');
   const [searchResults, setSearchResults] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [fadeAnim] = useState(new Animated.Value(0)); // Initial value for opacity: 0
+
+  useEffect(() => {
+    Animated.timing(
+      fadeAnim,
+      {
+        toValue: 1,
+        duration: 1000,
+        useNativeDriver: true,
+      }
+    ).start();
+  }, [searchResults]);
 
   const handleSearch = async () => {
     try {
       setLoading(true);
       // Make HTTP request to backend API using Axios
       const response = await axios.get(`https://levelart.up.railway.app/search/user?username=${searchQuery}`);
-      console.log("response:",response)
       setSearchResults(response.data.data); // Update this line
     } catch (error) {
       console.error('Error searching:', error.message);
@@ -20,36 +32,88 @@ const SearchScreen = () => {
       setLoading(false);
     }
   };
-  console.log('Search Results:', searchResults);
 
   return (
-    <View style={{ flex: 1, paddingHorizontal: 10 }}>
-      <TextInput
-        style={{ borderWidth: 1, borderColor: 'gray', padding: 10, marginBottom: 10 }}
-        placeholder="Search"
-        value={searchQuery}
-        onChangeText={setSearchQuery}
-        onSubmitEditing={handleSearch}
-      />
+    <View style={styles.container}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Search"
+          value={searchQuery}
+          onChangeText={setSearchQuery}
+          onSubmitEditing={handleSearch}
+        />
+        <TouchableOpacity onPress={handleSearch} style={styles.searchIconContainer}>
+          <Feather name="search" size={20} color="black" />
+        </TouchableOpacity>
+      </View>
       {loading ? (
-        <Text>Loading...</Text>
+        <Text style={styles.loadingText}>Loading...</Text>
       ) : (
         <FlatList
-        data={searchResults}
-        renderItem={({ item }) => (
-          // Render each search result item (user profile or post)
-          <View style={{ marginBottom: 10 }}>
-            <Text>{item.username}</Text> {/* Assuming username exists in the data */}
-            {/* Render additional information for each search result */}
-          </View>
-        )}
-        keyExtractor={(item) => item._id.toString()}
-      />
-      
-      
+          data={searchResults}
+          renderItem={({ item }) => (
+            <Animated.View style={{ ...styles.resultItem, opacity: fadeAnim }}>
+              <TouchableOpacity
+                onPress={() => console.log('Pressed on', item.username)} // Replace with desired action
+              >
+                <Text style={styles.username}>{item.username}</Text>
+              </TouchableOpacity>
+            </Animated.View>
+          )}
+          keyExtractor={(item) => item._id.toString()}
+        />
       )}
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 20,
+    paddingTop: 40,
+    backgroundColor: 'white',
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderWidth: 2,
+    borderColor: 'gray',
+    borderRadius: 25,
+    paddingHorizontal: 15,
+    marginBottom: 10,
+  },
+  input: {
+    flex: 1,
+    fontSize: 16,
+    paddingVertical: 10,
+    backgroundColor: '#fafafa',
+  },
+  searchIconContainer: {
+    marginLeft: 10,
+  },
+  loadingText: {
+    textAlign: 'center',
+    marginTop: 20,
+    fontSize: 16,
+    color: 'black',
+  },
+  resultItem: {
+    marginBottom: 10,
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+    borderWidth: 1,
+    borderColor: 'lightgray',
+    borderRadius: 25,
+    //borderRadius: 50,
+    backgroundColor: 'white',
+    elevation: 3,
+  },
+  username: {
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+});
 
 export default SearchScreen;
